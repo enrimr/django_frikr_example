686050464cb8dfbdb2a12be5e324dc6105b448ed
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-11 12:39:21 +0200

    Cambiado los labels del formulario de login y configurado el tipo de input a password en el password

--- frikrapp/photos/forms.py
+++ frikrapp/photos/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class LoginForm(forms.Form):
 
-    user_username = forms.CharField()
-    user_password = forms.CharField()
+    user_username = forms.CharField(label="Nombre de usuario")
+    user_password = forms.CharField(label="Password", widget=forms.PasswordInput())
