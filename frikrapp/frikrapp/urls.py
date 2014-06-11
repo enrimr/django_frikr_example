5986b6dddb9dc002da027216f8d5ed9ea4c37590
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-11 14:59:06 +0200

    Añadimos la ruta para crear foto

--- frikrapp/frikrapp/urls.py
+++ frikrapp/frikrapp/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 
     url(r'^login$', 'photos.views.user_login'),
     url(r'^logout$', 'photos.views.user_logout'),
-    url(r'^profile$', 'photos.views.user_profile')
+    url(r'^profile$', 'photos.views.user_profile'),
+    url(r'^create$', 'photos.view.create_photo')
 )
