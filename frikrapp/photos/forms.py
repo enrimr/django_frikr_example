2002fb57467de7e6fbee13bc296da2bae93544ca
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-11 14:59:26 +0200

    Creamos un formulario usando el modelo Photo de la base de datos

--- frikrapp/photos/forms.py
+++ frikrapp/photos/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from models import Photo
 
 class LoginForm(forms.Form):
 
     user_username = forms.CharField(label="Nombre de usuario")
     user_password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
+class PhotoForm(forms.ModelForm):
+    """
+    Pinta un formulario de una foto
+    """
+    class Meta:
+        model = Photo
