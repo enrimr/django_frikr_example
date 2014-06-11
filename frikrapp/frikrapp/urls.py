dcbf8703601163655066677518d31393dbd737de
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-11 16:24:05 +0200

    Hemos sustituido el método home(request) por una vista basada en clases. Editada ruta en urls.py y creado HomeView en views.py

--- frikrapp/frikrapp/urls.py
+++ frikrapp/frikrapp/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from photos import views
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Web URLs
-    url(r'^$', 'photos.views.home'),
+    url(r'^$', views.HomeView.as_view()),
     url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.photo_detail'),
     # photo_detail va a recibir un parametro llamado pk
     # que es un numero de 1 o mas cifras entre 0-9
--- frikrapp/photos/views.py
+++ frikrapp/photos/views.py
@@ -6,21 +6,24 @@ from django.contrib.auth import authenticate, login, logout
 from forms import LoginForm, PhotoForm
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
 
-def home(request): # En Django los controladores siempre reciben un objeto HttpRequest
-    """
-    Se ejecuta en /helloworld
-    :param request: objeto request
-    :return: objeto response
-    """
+class HomeView(View):
 
-    photo_list = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at')[:3]
+    def get(self, request): # En Django los controladores siempre reciben un objeto HttpRequest
+        """
+        Se ejecuta en /helloworld
+        :param request: objeto request
+        :return: objeto response
+        """
 
-    context = {
-        'photos': photo_list
-    }
+        photo_list = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at')[:3]
 
-    return render(request, 'photos/index.html', context)
+        context = {
+            'photos': photo_list
+        }
+
+        return render(request, 'photos/index.html', context)
 
 def photo_detail(request, pk):
     """
