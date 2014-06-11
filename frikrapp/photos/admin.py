a8aefc6658ddff9f5ca750de83da64a2b7da389d
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-11 18:30:38 +0200

    Hemos personalizado la columna owner para que en lugar de mostrar el nombre de usuario, muestre el Nombre Completo. Para ello, definimos un método que hemos decidido llamarlo owner_name. Así que en la lista de atributos personalizada, en lugar de 'owner' pondremos 'onwner_name'

--- frikrapp/photos/admin.py
+++ frikrapp/photos/admin.py
@@ -3,6 +3,9 @@ from models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'license', 'visibility', 'owner')
+    list_display = ('name', 'license', 'visibility', 'owner_name')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + ' ' + obj.owner.last_name
 
 admin.site.register(Photo, PhotoAdmin)
