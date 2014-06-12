8a11d19a292140f62c8e65353b54c0704da12563
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-12 10:34:55 +0200

    Añadido djangorestframework a nuestro proyecto en el settings.py

--- frikrapp/frikrapp/settings.py
+++ frikrapp/frikrapp/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos'
+    'photos',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
