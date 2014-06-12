648b9b22f23e22d95b6f7e6ad807481e2344ccce
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-12 15:21:10 +0200

    Añadimos validación de un atributo (description) al serializador de PhotoSerializer

--- frikrapp/photos/serializers.py
+++ frikrapp/photos/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password # para poder encriptar el password nuevo que generamos
+from django.conf import settings
 
 class UserSerializer(serializers.Serializer):
 
@@ -37,15 +38,31 @@ class UserSerializer(serializers.Serializer):
         existent_users = User.objects.filter(username=attrs.get('username'))
         if len(existent_users) > 0:
             raise serializers.ValidationError(u"Ya existe el usuario: " + attrs.get('username'))
-        return attrs # todo ha ido bien
+        return attrs # to_do ha ido bien
 
-    
+        # TODO
+        # hay que arreglar el hecho de que ya no nos deje crear usuarios nuevos: currrandonos el metodo put y haciendo
+        #  la validación a mano, o definiendo un serializer UserUpdateSerializer que herede de UserSerializer y
+        # hacer otro validate()
+
+
+# Permite que BADWORDS se pueda reescribir desde el settings.py
+BADWORDS = getattr(settings, 'BADWORDS', ())
+
+# Las importaciones no tienen por qué ser al principio del fichero
 from models import Photo
 
 class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
+
+    def validate_description(self, attrs, source):
+        description = attrs.get(source)
+        for badword in BADWORDS:
+            if badword in description:
+                raise serializers.ValidationError(badword + u" no está permitida")
+        return attrs # Ha ido bien
 
 class PhotoListSerializer(PhotoSerializer):
 
