fea0a40f1ebaf3f1311bb0b172520dc6493cbabc
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-06-12 12:19:50 +0200

    Añadido método crear usuario

--- frikrapp/photos/api.py
+++ frikrapp/photos/api.py
@@ -10,3 +10,11 @@ class UserListAPI(APIView):
         users = User.objects.all() # recuperamos todos los usuarios de la base de datos
         serializer = UserSerializer(users, many=True) # transforma cada usuario de la lista en una lista de diccionarios
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.DATA)
+        if serializer.is_valid(): # los serializer funcionan como los djangoforms, así que hay que validarlo
+            serializer.save()     # guardamos en la base de datos el nuevo usuario
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
